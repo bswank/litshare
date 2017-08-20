a7f10d55d8f1e0493564c885028f5349b6c9eb59
Brian Swank <brianswank@gmail.com>
2017-08-20 15:07:28 -0400

    Working on Book Search API

---
+++ controllers/libraryController.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const Community = mongoose.model('Community');
+const Library = mongoose.model('Library');
+const promisify = require('es6-promisify');
+
+exports.searchLibraries = (req, res) => {
+  res.render('libraries', { title:
+  'Search for Books' });
+};
